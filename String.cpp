44202191244d15671c7ab6b61cde81012f1e6c9c
RostislavStoyanov <rostislav.stoyanov@hotmail.com>
2017-07-11 18:54:38 +0300

    editing

--- String.cpp
+++ String.cpp
@@ -14,6 +14,7 @@ String::String() {
         std::cerr << "Not enough memory" << std::endl;
         throw;
     }
+	data[0] = 0;
 }
 
 String::~String() {
@@ -27,11 +28,13 @@ size_t String::length() const {
 String::String(const String &other) {
     this->maxSize = other.maxSize;
     getData(other.data, maxSize);
+	terminate();
 }
 
 String::String(const char *other) {
     this->maxSize = strlen(other);
     getData(other, maxSize);
+	terminate();
 }
 
 void String::getData(const char *dataSource, size_t maxSize) {
@@ -48,10 +51,23 @@ void String::getData(const char *dataSource, size_t maxSize) {
     }
 }
 
+void String::terminate()
+{
+	if (currentSize + 1 != maxSize)
+	{
+		data[currentSize + 1] = 0;
+	}
+	else {
+		addChar(0);
+		currentSize--;
+	}
+}
+
 String String::operator=(const String &other) {
     if (this != &other) {
         maxSize = other.maxSize;
         getData(other.data, maxSize);
+		terminate();
     }
     return *this;
 }
@@ -60,13 +76,59 @@ String String::operator=(const char *other) {
     if (this->data != other) {
         maxSize = strlen(other);
         getData(other, maxSize);
+		terminate();
     }
     return *this;
 }
 
 void String::addChar(const char newChar) {
-    if (maxSize == currentSize) {
-
+    if (maxSize == currentSize+1) {
+		maxSize *= 2;
+		getData(this->data, maxSize);
     }
+	data[currentSize++] = newChar;
+	terminate();
+}
+
+void String::getLine(std::ifstream & is, const char delim='\n')
+{
+	char temp;
+	while (!is.eof())
+	{
+		is.get(temp);
+		if (temp == delim)
+			break;
+		else
+			addChar(temp);
+	}
+	return;
+}
+
+size_t String::find(const char * text)
+{
+	size_t currPos=-1;
+	bool found = 0;
+	for (size_t i = 0; i < currentSize; i++)
+	{
+		if (data[i] = text[0])
+		{
+			for (size_t j = i+1; j < currentSize; j++)
+			{
+				if (data[j] = text[j - i])
+					found = 1;
+				else
+				{
+					found = 0;
+					break;
+				}
+			}
+			if (found == 1)
+			{
+				currPos = i;
+				break;
+			}
+		}
+	}
+	return currPos;
 }
 
--- String.h
+++ String.h
@@ -1,20 +1,23 @@
 //
 // Created by rosti on 10.07.17.
 //
-
+#pragma warning(disable:4996)
 #ifndef STRING_STRING_H
 #define STRING_STRING_H
 
 #include<iostream>
 #include<cstring>
+#include<fstream>
 
 class String {
 private:
     char *data; //holds the text
     size_t maxSize; //maximum number of chars in data
     size_t currentSize; //current number of chars in data
+
     void getData(const char *, size_t maxSize); //sets currentSize to the other char* size and
-    // copies the content of the other char* to data
+												// copies the content of the other char* to data
+	void terminate(); //terminates the char array
 public:
     String(); //default constructor
     ~String(); //destructor
@@ -28,6 +31,10 @@ public:
     size_t length() const; //currentSize getter
 
     void addChar(const char); //adds a char to the data array
+
+	void getLine(std::ifstream&,const char); // reads line till deliminator and stores it in this string object(all data previously stored is lost)
+
+	size_t find(const char*); //searches for text in the string and if found returns the starting position , if not found returns -1;
 };
 
 
