a7ae64bb7ef8e5357b593770d1cd463a1d05ecbe
RostislavStoyanov <rostislav.stoyanov@hotmail.com>
2017-07-12 19:23:04 +0300

    editing

--- main.cpp
+++ main.cpp
@@ -2,5 +2,13 @@
 
 int main() {
 
+	char* fileName=new char[100];
+	std::cin >> fileName;
+	std::ifstream is(fileName, std::ios::in);
+	String str;
+	str.getLine(is, '.');
+	str.print();
+	is.close();
+	delete[] fileName;
     return 0;
 }
